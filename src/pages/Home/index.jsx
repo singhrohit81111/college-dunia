17bcd60ee36a2693ee2e905f4144c1fe1f1def61
Rohit Singh <singhrohit81111@gmail.com>
2024-12-08 00:04:04 +0530

    feat: implemented intersection observer

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import Table from "@components/common/Table";
-import data from "../../config/data.json";
+import data from "@config/data.json";
 
 function Home() {
   const [colleges, setColleges] = useState([]);
@@ -8,17 +8,25 @@ function Home() {
   const observerRef = useRef();
 
   useEffect(() => {
-    const slicedColleges = data.slice(0, 10);
+    const slicedColleges = data.slice(0, colleges.length + 10);
     console.log(slicedColleges, "slice");
     setColleges(slicedColleges);
   }, []);
 
   useEffect(() => {
+    function loadData() {
+      setTimeout(() => {
+        const slicedColleges = data.slice(colleges.length, colleges.length + 10);
+        console.log(slicedColleges, "triggered");
+        setColleges(prevColleges=>([...prevColleges,...slicedColleges]));
+      }, 3 * 1000);
+    }
+
     observerRef.current = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            //   loadData();
+              loadData();
             console.log("hii");
           }
         });
@@ -34,7 +42,7 @@ function Home() {
   return (
     <main className="w-screen">
       <section className="p-6">
-        <Table colleges={colleges}/>
+        <Table colleges={colleges} />
       </section>
       <section ref={targetRef} />
     </main>
