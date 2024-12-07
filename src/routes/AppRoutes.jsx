b41a1803b0109edd8527b3bdd8b36a30aa105c34
Rohit Singh <singhrohit81111@gmail.com>
2024-12-07 15:50:46 +0530

    feat: setup initial files and folders

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import  Home  from "../pages/Home";
+import { HOME } from "../config/pathNames";
+
+
+
+{/*V6 routing*/}
+export const routes=createBrowserRouter([
+    {
+        path:HOME,
+        element:<Home/>,
+        errorElement:""
+    }
+])
