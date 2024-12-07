34ab34c24966f0d05d6b0bddd737742fc547e4c3
Rohit Singh <singhrohit81111@gmail.com>
2024-12-07 18:31:27 +0530

    feat: added static image url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,3 @@
-export const TABLE_HEADINGS=['CD Rank','Collges','Course Fees','Placement','User Reviews','Rankings']
+export const TABLE_HEADINGS=['CD Rank','Collges','Course Fees','Placement','User Reviews','Rankings'];
+
+export const IMAGE_URL='https://image-static.collegedunia.com/public/college_data/images/logos/1434977143jpjpjpjpj.jpg?h=43&w=43&mode=stretch'
